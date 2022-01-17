e3dee77bf896a86483a6e8e79fb4d91be7e0bc13
khoaphamce <phamkhoaa18@gmail.com>
2022-01-17 08:49:12 +0700

    Listing post to home page

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -4,7 +4,14 @@ const { multipleMongooseToObject, ...rest } = require('../../utility/mongoose');
 class SiteController {
     //[GET] /
     home(req, res, next) {
-        res.render('home');
+        Post.find().sort({createdAt: -1}).limit(3)
+            .then((result) => {
+                console.log("All blog:", result);
+                res.render("home", {breaking_post: multipleMongooseToObject(result)});    
+            })
+            .catch(err=>{
+                res.render("home", {breaking_post: []});    
+            })
     }
 }
 
