8bf83a88cf9b638a455fbd92810d4efceacebec6
ledong0110 <74060032+ledong0110@users.noreply.github.com>
2022-02-07 23:59:05 +0700

    Change a little config

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 const { auth } = require('express-openid-connect');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const route = require('./routes');
 const db = require('./config/db');
